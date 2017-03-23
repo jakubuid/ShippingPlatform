a882ebbbb9ec12cb11e448416a68e03b27730de5
JakubWitczak <jakubwitczak6@gmail.com>
2017-03-23 09:17:18 -0700

    added dapper configuration in WebApiConfig

--- WebService/App_Start/WebApiConfig.cs
+++ WebService/App_Start/WebApiConfig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Web.Http;
+using ShippingPlatform.DataBase;
 
 namespace WebService
 {
@@ -10,6 +11,8 @@ namespace WebService
     {
         public static void Register(HttpConfiguration config)
         {
+            DapperConfiguration.Configure();
+
             // Web API configuration and services
             config.Formatters.JsonFormatter.SupportedMediaTypes.Add(
                 new MediaTypeHeaderValue("text/html"));
